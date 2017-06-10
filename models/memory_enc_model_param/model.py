4db53a95f472f2c3d21d8b7190082edbc57b4301
Amani Peddada <amanivp@stanford.edu>
2017-06-10 11:27:19 -0700

    Added cell argument to test model

--- models/memory_enc_model_param/model.py
+++ models/memory_enc_model_param/model.py
@@ -340,3 +340,11 @@ class ASRModel:
 										labels=test_targets)
 		test_preds = sess.run(self.decoded, feed_dict = feed_dict)
 		return None, test_preds
+
+
+	# Tests on a single batch of data
+	def get_memory(self, sess, test_inputs, test_seq_len, test_targets):
+		feed_dict = self.create_feed_dict(inputs=test_inputs, seq_lens=test_seq_len,\
+										labels=test_targets)
+		init_memory = sess.run(self.init_memory, feed_dict = feed_dict)
+		return init_memory
