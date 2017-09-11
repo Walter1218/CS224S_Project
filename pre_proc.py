029870252f6ec12c6a9058f14d58820f0c3e7382
lmkostas <lmkostas@stanford.edu>
2017-09-11 13:31:30 -0700

    new log melspectrogram features

--- pre_proc.py
+++ pre_proc.py
@@ -8,14 +8,14 @@ SEED = 472
 PCT_TRAIN =.70
 PCT_DEV = .15
 PCT_TEST = .15
-hop_length = 512
+SAMPLE_RATE = 16000
+N_MELS = 40
+hop_length = 160
 n_mfcc = 39
 
 
 def split_and_save_data(dataset):
 
-	if dataset == 'wsj0_si':
-		dirname = "data\\wsj0_raw_data\\**\\wsj0\\si_tr_s\\**\\*.wv1"
 	dirname1 = "data\\wsj0_raw_data\\**\\wsj0\\s*_tr_*\\**\\*.wv1"
 	dirname2 = "data\\wsj0_raw_data\\**\\wsj0\\s*_dt_*\\**\\*.wv1"
 	dirname3 = "data\\wsj0_raw_data\\**\\wsj0\\s*_et_*\\**\\*.wv1"
@@ -56,19 +56,26 @@ def split_and_save_data(dataset):
 	print "Test Files:", len(data_split['test'])
 
 	for i,f in enumerate(fp):
-		if i%100==0: print (i)
+		if i%100==0: print i
 		filename = re.search('\w+.wv1', f).group(0).split('.')[0]
 		fp_to_id[filename] = i
 		id_to_fp[i] = filename
 		if i in data_split['train']:
-			y, sr = librosa.load(f)
-			mfcc_train[i] = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=n_mfcc)
+			y, sr = librosa.load(f, sr=SAMPLE_RATE)
+			# mfcc_train[i] = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=n_mfcc)
+			S = librosa.feature.melspectrogram(y=y, sr=sr, hop_length=hop_length, n_mels=N_MELS)
+			print S.shape, np.log(S).shape
+			print np.log(S)
+			print np.log(librosa.feature.melspectrogram(y=y, sr=sr, hop_length=hop_length, n_mels=N_MELS))
+			mfcc_train[i] = np.log(librosa.feature.melspectrogram(y=y, sr=sr, hop_length=hop_length, n_mels=N_MELS))
 		elif i in data_split['dev']:
-			y, sr = librosa.load(f)
-			mfcc_dev[i] = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=n_mfcc)
+			y, sr = librosa.load(f, sr=SAMPLE_RATE)
+			# mfcc_dev[i] = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=n_mfcc)
+			mfcc_dev[i] = np.log(librosa.feature.melspectrogram(y=y, sr=sr, hop_length=hop_length, n_mels=N_MELS))
 		else:
-			y, sr = librosa.load(f)
-			mfcc_test[i] = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=n_mfcc)
+			y, sr = librosa.load(f, sr=SAMPLE_RATE)
+			# mfcc_test[i] = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=n_mfcc)
+			mfcc_test[i] = np.log(librosa.feature.melspectrogram(y=y, sr=sr, hop_length=hop_length, n_mels=N_MELS))
 
 	print "Done Processing Files!"
 	print "Total Files Processed:", len(fp)
@@ -126,6 +133,8 @@ def split_and_save_transcripts(dataset, data_split, fp_to_id):
 	labels_dev = {}
 	labels_test = {}
 
+	no_id = {}
+
 	comp_tr = set(data_split['train'])
 	comp_dev = set(data_split['dev'])
 	comp_ts = set(data_split['test'])
@@ -144,7 +153,9 @@ def split_and_save_transcripts(dataset, data_split, fp_to_id):
 				if filename in fp_to_id:
 					id_no = fp_to_id[filename]
 				else:
+					no_id[filename] = f
 					print f, filename
+					continue
 
 				num_string = []
 				for char in transcript:
@@ -162,17 +173,6 @@ def split_and_save_transcripts(dataset, data_split, fp_to_id):
 				else:
 					labels_test[id_no] = num_string
 					if id_no in comp_ts: comp_ts.remove(id_no)
-	
-	itof = pickle.load(open('data\\wsj0_si\\id_no_to_filepath.pkl', 'rb'))
-	print 'Train'
-	for f in comp_tr:
-		print itof[f]
-	print 'Dev'
-	for f in comp_dev:
-		print itof[f]
-	print 'Test'
-	for f in comp_ts:
-		print itof[f]
 
 	print "Done Processing Files!"
 	print "Train Files:", len(labels_train)
@@ -195,6 +195,11 @@ def split_and_save_transcripts(dataset, data_split, fp_to_id):
 	pickle.dump(labels_test, labels_test_pkl)
 	labels_test_pkl.close()
 	print "Test set labels features saved to data\\"+dataset+"\\labels_test.pkl"
+
+	no_id_pkl = open('data\\'+dataset+'\\no_id.pkl', 'wb')
+	pickle.dump(no_id, no_id_pkl)
+	no_id_pkl.close()
+	print "Label id's with no data saved to data\\"+dataset+"\\no_id.pkl"
 
 	print "All Done!"
 
